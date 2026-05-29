03e6ed00a6a47eae4e39adc8f35e4f1b16573fe1
dashrath chauhan <dashrath.chauhan@ga4gh.org>
2026-05-29 12:05:39 +0100

    Changed config to ENV vars

--- app/config.py
+++ app/config.py
@@ -3,7 +3,10 @@ import os
 class Config:
 
     def __init__(self):
-        self.api_base_url = os.getenv("GA4GH_ANALYTICS_DASHBOARD_API_BASE_URL", "http://localhost:8000")
-        self.ui_port = int(os.getenv("GA4GH_ANALYTICS_DASHBOARD_UI_PORT", 8050))
+        self.api_base_url = os.getenv(
+            "GA4GH_ANALYTICS_DASHBOARD_API_BASE_URL",
+            "http://localhost:8000",
+        ).rstrip("/")
+        self.ui_port = int(os.getenv("GA4GH_ANALYTICS_DASHBOARD_UI_PORT", "8050"))
 
 config = Config()
